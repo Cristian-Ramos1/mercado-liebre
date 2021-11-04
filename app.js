f03b9423771dbc01a93ae4f6ab023a9b438c8c00
Cristian <cristianramos92639@gmail.com>
2021-11-04 20:00:47 -0300

    3er commit

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("Se prendio el server")
-})
+app.listen(process.env.PORT || 3000)
 
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "views/home.html"))
